c73bec82c1acc2bb25e1899f025ac7d4387cacc5
rafaelsene01 <rafaelssene@gmail.com>
2020-08-03 22:21:14 -0300

    limpando projeto

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,30 +1,30 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
-import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
     path: "/",
-    name: "Home",
-    component: Home
+    name: "Usuarios",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Usuarios.vue"),
   },
   {
-    path: "/about",
-    name: "About",
+    path: "/Produtos",
+    name: "Produtos",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+      import(/* webpackChunkName: "about" */ "../views/Produtos.vue"),
+  },
 ];
 
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
-  routes
+  routes,
 });
 
 export default router;
---
+++ src/views/Produtos.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="about"></div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({})
+export default class Produtos extends Vue {}
+</script>
